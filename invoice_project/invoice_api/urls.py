365a5b33bebcacb993704ac39a54394a7ca4230b
Huzaifa Ahmad <huzaifaahmad5535@gmail.com>
2025-05-22 11:59:58 +0500

    Delete invoice_project directory

--- invoice_project/invoice_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import generate_invoice,invoice_form_view
-
-urlpatterns = [
-    path('', invoice_form_view, name='invoice-form'),
-    path("generate-invoice/", generate_invoice, name='generate-invoice'),
-
-]
--- invoice_project/invoice_project/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def invoice_form_view(request):
-    return render(request, "invoice_form.html")
